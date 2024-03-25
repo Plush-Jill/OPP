d0413268cda3f071671632fe52e35c9439e78559
Plush-Jill <bockarevvlad23@gmail.com>
2024-03-26 00:14:49 +0700

    Final const immutable.

--- Lab_2/V1.c
+++ Lab_2/V1.c
@@ -1,92 +1,117 @@
+#include <math.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <stdbool.h>
 
-#define N 15000
-#define EPSILON 1E-7
+
+#define N 16000
+#define EPSILON 1E-10
 #define TAU 1E-5
-#define MAX_ITERATION_COUNT 100000
+#define MAX_ITERATION_COUNT 1000000
 
+void initMatrixA_v1(double* matrixA, int size);
+void initVectorX_v1(double* vectorX, int size);
+void initVectorB_v1(double* vectorB, int size);
+double getNormSquare_v1(const double* vector, int size);
+void calcVectorAXB_v1(const double* matrixA, const double* vectorX, const double* vectorB, double* vectorAXB, int size);
+void calcNextVectorX_v1(const double* vectorAXB, double* vectorX, double tau, int size);
+bool checkResultVector_v1 (const double* vectorX);
 
-void initMatrixA_v1 (double* matrixA){
-    for (int i = 0; i < N; ++i){
-        for (int j = 0; j < N; ++j){
-            matrixA[i * N + j] = 1;
-            if (i == j){
-                matrixA[i * N + j] = 2;
-            }
-        }
-    }
-}
-void initVectorX_v1 (double* vectorX){
-    memset(vectorX, 0, N * sizeof(double));
-}
-void initVectorB1_v1 (double* vectorB){
-    memset(vectorB, N + 1, N * sizeof(double));
-}
-void calcAxb_v1 (double* vectorAxb, const double* matrixA, const double* vectorX, const double* vectorB){
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < N; ++j) {
-            vectorAxb[i] += matrixA[i * N + j] * vectorX[j];
-        }
-        vectorAxb[i] -= vectorB[i];
-    }
-}
-void setVectorXtoFofX_v1 (double* vectorX, const double* vectorAxb){
-    for (int i = 0; i < N; ++i){
-        vectorX[i] -= TAU * (vectorAxb[i]);
-    }
-}
-double calculateEuclideanVectorNorm_v1 (const double* vector){
-    double norm = 0;
-    for (int i = 0; i < N; ++i){
-        norm += vector[i] * vector[i];
-    }
-    return norm;
-}
-double calculateSquareRootOfNorm_v1 (const double* vector) {
-    return sqrt(calculateEuclideanVectorNorm_v1(vector));
-}
-void printVector_v1 (double* vector){
-    printf("(");
-    for (int i = 0; i < N; ++i){
-        i == N - 1 ? printf("%lf)", vector[i]) : printf("%lf, ", vector[i]);
-    }
-    //printf(")\n");
-}
-
-
-int main_v1() {
-    int iterationsCount;
+int main() {
+    double* matrixA = malloc(sizeof(double) * N * N);
     double* vectorX = malloc(sizeof(double) * N);
     double* vectorB = malloc(sizeof(double) * N);
-    double* vectorAxb = malloc(sizeof(double) * N);
-    double* matrixA = malloc(sizeof(double) * N * N);
-    double vectorAxbEuclideanNorm;
-    double vectorBEuclideanNorm;
-    double accuracy = N;
+    double* vectorAXB = malloc(sizeof(double) * N);
 
-    initVectorX_v1(vectorX);
-    initVectorB1_v1(vectorB);
-    initMatrixA_v1(matrixA);
-    double beginningTime;
-    double endingTime;
+    initMatrixA_v1(matrixA, N);
+    initVectorX_v1(vectorX, N);
+    initVectorB_v1(vectorB, N);
 
+    double vectorBNorm = sqrt(getNormSquare_v1(vectorB, N));
 
-    for (iterationsCount = 0; accuracy >= EPSILON && iterationsCount < MAX_ITERATION_COUNT; ++iterationsCount){
+    fprintf(stdout, "Threads count: %d\n", omp_get_max_threads());
 
-        calcAxb_v1(vectorAxb, matrixA, vectorX, vectorB);
-        setVectorXtoFofX_v1(vectorX, vectorAxb);
-        vectorAxbEuclideanNorm = calculateEuclideanVectorNorm_v1(vectorAxb);
-        vectorBEuclideanNorm = calculateEuclideanVectorNorm_v1(vectorB);
-        accuracy = sqrt(vectorAxbEuclideanNorm / vectorBEuclideanNorm);
+    double beginningTime = omp_get_wtime();
 
+    int i;
+    double accuracy = EPSILON + 1;
+    for (i = 0; accuracy > EPSILON && i < MAX_ITERATION_COUNT; ++i) {
+        calcVectorAXB_v1(matrixA, vectorX, vectorB, vectorAXB, N);
+        calcNextVectorX_v1(vectorAXB, vectorX, TAU, N);
+        accuracy = sqrt(getNormSquare_v1(vectorAXB, N)) / vectorBNorm;
     }
-    printVector_v1(vectorX);
 
+    double endingTime = omp_get_wtime();
+
+
+    fprintf(stdout, "Time = %.10lf sec\n", endingTime - beginningTime);
+    if (checkResultVector_v1(vectorX)) {
+        printf("Vector is calculated correctly.\n");
+    } else {
+        printf("Vector is calculated incorrectly.\n");
+    }
+
+    free(matrixA);
     free(vectorX);
     free(vectorB);
-    free(vectorAxb);
-    free(matrixA);
+    free(vectorAXB);
+
+    return 0;
+}
+
+void initMatrixA_v1(double* matrixA, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            matrixA[i * size + j] = 1;
+        }
+        matrixA[i * size + i] = 2;
+    }
+}
+void initVectorX_v1(double* vectorX, int size) {
+    for (int i = 0; i < size; ++i) {
+        vectorX[i] = 0;
+    }
+}
+void initVectorB_v1(double* vectorB, int size) {
+    for (int i = 0; i < size; ++i) {
+        vectorB[i] = N + 1;
+    }
+}
+double getNormSquare_v1(const double* vector, int size) {
+    double normSquare = 0;
+
+    #pragma omp parallel for schedule(runtime, 50) reduction(+:normSquare)
+    for (int i = 0; i < size; ++i) {
+        normSquare += vector[i] * vector[i];
+    }
+    return normSquare;
+}
+void calcVectorAXB_v1(const double* matrixA, const double* vectorX, const double* vectorB, double* vectorAXB, int size) {
+    #pragma omp parallel for schedule(runtime, 50)
+    for (int i = 0; i < size; ++i) {
+        vectorAXB[i] = -vectorB[i];
+        for (int j = 0; j < N; ++j)
+            vectorAXB[i] += matrixA[i * N + j] * vectorX[j];
+    }
+}
+void calcNextVectorX_v1(const double* vectorAXB, double* vectorX, double tau, int size) {
+    #pragma omp parallel for schedule(runtime, 50)
+    for (int i = 0; i < size; ++i) {
+        vectorX[i] -= tau * vectorAXB[i];
+    }
+
+
+}
+bool checkResultVector_v1 (const double* vectorX) {
+    double preEpsilon = 1 - EPSILON;
+    double postEpsilon = 1 + EPSILON;
+
+    for (int i = 0; i < N; ++i) {
+        if (!(vectorX[i] < postEpsilon && vectorX[i] > preEpsilon)) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- Lab_2/V2.c
+++ Lab_2/V2.c
@@ -1,140 +1,154 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <math.h>
 #include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define N 15000
-#define EPSILON 1E-7
+#define N 16000
+#define EPSILON 1E-10
 #define TAU 1E-5
-#define MAX_ITERATION_COUNT 100000
+#define MAX_ITERATION_COUNT 1000000
 
+void setLineCountsAndOffsets_v2(int* lineCounts, int* line_offsets, int size, int thread_count);
+void initMatrixA_v2(double* matrixA, int size);
+void initVectorX_v2(double* vectorX, int size);
+void initVectorB_v2(double* vectorB, int size);
+double getNormSquare_v2(const double* vector, int size);
+void calcVectorAXB_v2(const double* matrixA, const double* vectorX, const double* vectorB, double* vectorAXB, int size);
+void calcNextVectorX_v2(const double* vectorAXB, double* vectorX, double tau, int size);
+bool checkResultVector_v200 (const double* vectorX) {
+    double preEpsilon = 1 - EPSILON;
+    double postEpsilon = 1 + EPSILON;
 
-void initMatrixA_v2 (double* matrixA){
-    for (int i = 0; i < N; ++i){
-        for (int j = 0; j < N; ++j){
-            matrixA[i * N + j] = 1;
-            if (i == j){
-                matrixA[i * N + j] = 2;
-            }
+    for (int i = 0; i < N; ++i) {
+        if (!(vectorX[i] < postEpsilon && vectorX[i] > preEpsilon)) {
+            return false;
         }
     }
-}
-void initVectorX_v2 (double* vectorX){
-    memset(vectorX, 0, N * sizeof(double));
-}
-void initVectorB_v2 (double* vectorB){
-    memset(vectorB, N + 1, N * sizeof(double));
-}
-void calcAxb_v2 (double* vectorAxb, const double* matrixA, const double* vectorX, const double* vectorB, int currentProcessSize){
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < N; ++j) {
-            vectorAxb[i] += matrixA[i * N + j] * vectorX[j];
-        }
-        vectorAxb[i] -= vectorB[i];
-    }
-}
-void setVectorXtoFofX_v2 (double* vectorX, const double* vectorAxb, int sizeInCurrentProcess){
-    for (int i = 0; i < N; ++i){
-        vectorX[i] -= TAU * (vectorAxb[i]);
-    }
-}
-double calculateEuclideanVectorNorm_v2 (const double* vector, int currentProcessSize){
-    double norm = 0;
-    for (int i = 0; i < N; ++i){
-        norm += vector[i] * vector[i];
-    }
-    return norm;
-}
-double calculateSquareRootOfNorm_v2 (const double* vector, int currentProcessSize) {
-    return sqrt(calculateEuclideanVectorNorm_v2(vector, currentProcessSize));
-}
-void printVector_v2 (double* vector){
-    printf("(");
-    for (int i = 0; i < N; ++i){
-        i == N - 1 ? printf("%lf)", vector[i]) : printf("%lf, ", vector[i]);
-    }
-    //printf(")\n");
-}
-void initLineCountsAndOffsets_v2(int* lineCountsPerProcess, int* lineOffsets, int processSize) {
-    int offset = 0;
-    for (int i = 0; i < processSize; ++i) {
-        lineCountsPerProcess[i] = N / processSize;
 
-        if (i < N % processSize) {
-            ++lineCountsPerProcess[i];
-        }
-        lineOffsets[i] = offset;
-        offset += lineCountsPerProcess[i];
-    }
+    return true;
 }
 
-
-int main_v0() {
-    int iterationsCount;
+int main() {
+    double* matrixA = malloc(sizeof(double) * N * N);
     double* vectorX = malloc(sizeof(double) * N);
     double* vectorB = malloc(sizeof(double) * N);
-    double* vectorAxb = malloc(sizeof(double) * N);
-    double* matrixA = malloc(sizeof(double) * N * N);
-    double vectorAxbEuclideanNorm;
-    double vectorBEuclideanNorm;
-    double accuracy = N;
+    double* vectorAXB = malloc(sizeof(double) * N);
 
-    int threadID;
-    int threadCount = omp_get_max_threads();
-    int* lineCounts = calloc(threadCount, sizeof(int));
-    int* lineOffsets = calloc(threadCount, sizeof(int));
+    initMatrixA_v2(matrixA, N);
+    initVectorX_v2(vectorX, N);
+    initVectorB_v2(vectorB, N);
 
-    initLineCountsAndOffsets_v2(lineCounts, lineOffsets, threadCount);
-    initVectorX_v2(vectorX);
-    initVectorB_v2(vectorB);
-    initMatrixA_v2(matrixA);
 
-    double beginningTime;
-    double endingTime;
+    double vectorBNorm = sqrt(getNormSquare_v2(vectorB, N));
 
-    beginningTime = omp_get_wtime();
-    double vectorAxbNormParts[threadCount];
-    vectorBEuclideanNorm = calculateEuclideanVectorNorm_v2(vectorB, N);
 
-    fprintf(stdout, "Threads count: %d\n", threadCount);
-    #pragma omp parallel private(threadID)
+    int i = 0;
+    double accuracy = EPSILON;
+    int currentThreadCount;
+    int* lineCounts;
+    int* lineOffsets;
+
+    double beginningTime = omp_get_wtime();
+
+    #pragma omp parallel shared(lineCounts, lineOffsets)
     {
-        threadID = omp_get_thread_num();
+        currentThreadCount = omp_get_num_threads();
+        int threadID = omp_get_thread_num();
 
-        for (iterationsCount = 0; accuracy >= EPSILON && iterationsCount < MAX_ITERATION_COUNT; ++iterationsCount) {
-            calcAxb_v2(vectorAxb + lineOffsets[threadID],
-                       matrixA + lineOffsets[threadID] * N,
-                       vectorX,
-                       vectorB + lineOffsets[threadID],
-                       lineCounts[threadID]);
+        lineCounts = malloc(sizeof(int) * currentThreadCount);
+        lineOffsets = malloc(sizeof(int) * currentThreadCount);
+        setLineCountsAndOffsets_v2(lineCounts, lineOffsets, N, currentThreadCount);
 
-            // #pragma omp barrier
-            setVectorXtoFofX_v2(vectorX + lineOffsets[threadID], vectorAxb + lineOffsets[threadID],
-                                lineCounts[threadID]);
+        #pragma omp single
+        {
+            fprintf(stdout, "currentThreadCount: %d\n", currentThreadCount);
+        };
+        for (; accuracy >= EPSILON && i < MAX_ITERATION_COUNT; ++i) {
+            calcVectorAXB_v2(matrixA + lineOffsets[threadID] * N, vectorX, vectorB + lineOffsets[threadID],
+                             vectorAXB + lineOffsets[threadID], lineCounts[threadID]);
+            #pragma omp barrier
 
-            vectorAxbNormParts[threadID] = calculateEuclideanVectorNorm_v2(vectorAxb + lineOffsets[threadID],
-                                                                           lineCounts[threadID]);
+            calcNextVectorX_v2(vectorAXB + lineOffsets[threadID], vectorX + lineOffsets[threadID], TAU,
+                               lineCounts[threadID]);
+
+            #pragma omp single
             accuracy = 0;
 
             #pragma omp atomic
-            vectorAxbEuclideanNorm += vectorAxbNormParts[threadID];
-
+            accuracy += getNormSquare_v2(vectorAXB + lineOffsets[threadID], lineCounts[threadID]);
             #pragma omp barrier
+
             #pragma omp single
-            accuracy = sqrt(vectorAxbEuclideanNorm / vectorBEuclideanNorm);
-
+            accuracy = sqrt(accuracy) / vectorBNorm;
+        }
     }
+
+    double endingTime = omp_get_wtime();
+
+    fprintf(stdout, "Time = %.10lf sec\n", endingTime - beginningTime);
+    if (checkResultVector_v200(vectorX)) {
+        printf("Vector is calculated correctly.\n");
+    } else {
+        printf("Vector is calculated incorrectly.\n");
     }
-    endingTime = omp_get_wtime();
-    fprintf(stdout, "Time: %lf sec.\n", endingTime - beginningTime);
-    printVector_v2(vectorX);
 
-
-    free(lineOffsets);
     free(lineCounts);
+    free(lineOffsets);
+    free(matrixA);
     free(vectorX);
     free(vectorB);
-    free(vectorAxb);
-    free(matrixA);
+    free(vectorAXB);
+
+    return 0;
+}
+
+void setLineCountsAndOffsets_v2(int* lineCounts, int* line_offsets, int size, int thread_count) {
+    int offset = 0;
+    for (int i = 0; i < thread_count; ++i)
+    {
+        lineCounts[i] = size / thread_count;
+
+        if (i < size % thread_count) {
+            ++lineCounts[i];
+        }
+        line_offsets[i] = offset;
+        offset += lineCounts[i];
+    }
+}
+void initMatrixA_v2(double* matrixA, int size) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            matrixA[i * size + j] = 1;
+        }
+        matrixA[i * size + i] = 2;
+    }
+}
+void initVectorX_v2(double* vectorX, int size) {
+    for (int i = 0; i < size; ++i) {
+        vectorX[i] = 0;
+    }
+}
+void initVectorB_v2(double* vectorB, int size) {
+    for (int i = 0; i < size; ++i)
+        vectorB[i] = N + 1;
+}
+double getNormSquare_v2(const double* vector, int size) {
+    double res = 0.0;
+    for (int i = 0; i < size; ++i)
+        res += vector[i] * vector[i];
+
+    return res;
+}
+void calcVectorAXB_v2(const double* matrixA, const double* vectorX, const double* vectorB, double* vectorAXB, int size) {
+    for (int i = 0; i < size; ++i) {
+        vectorAXB[i] = -vectorB[i];
+        for (int j = 0; j < N; ++j)
+            vectorAXB[i] += matrixA[i * N + j] * vectorX[j];
+    }
+}
+void calcNextVectorX_v2(const double* vectorAXB, double* vectorX, double tau, int size) {
+    for (int i = 0; i < size; ++i) {
+        vectorX[i] -= tau * vectorAXB[i];
+    }
 }
