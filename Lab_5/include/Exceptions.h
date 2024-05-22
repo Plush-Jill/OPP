38055057c2592da3c0aa185c600ee1b5837f1121
Plush-Jill <bockarevvlad23@gmail.com>
2024-05-22 16:16:16 +0700

    Final.

--- Lab_5/include/Exceptions.h
+++ Lab_5/include/Exceptions.h
@@ -1,17 +1,6 @@
 #include <exception>
 #include <string>
 
-class TaskQueueException : std::exception{
-private:
-    std::string message_;
-public:
-    explicit TaskQueueException(std::string  message) : message_(std::move(message)) {}
-    explicit TaskQueueException() = default;
-    virtual const char* what() const noexcept{
-        return message_.c_str();
-    }
-};
-
 class InitMPIException : std::exception{
 private:
     std::string message_;
